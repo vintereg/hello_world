0338719ef6d43153ae0f460e16b4cddc0110c42e
vintereg <vintereg@users.noreply.github.com>
2015-03-16 14:59:50 -0400

    Add this file to GitHup

---
+++ src/org/vegan/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vegan;
+
+/**
+ * @author egan
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git!");
+
+	}
+
+}
