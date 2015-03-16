859d9b598021fbda9a5733c0fa4eac94e431e752
vintereg <vintereg@users.noreply.github.com>
2015-03-16 19:49:40 -0400

    add correct files to pizza.

---
+++ src/org/vegan/pizza/NewYorkPizza.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package org.vegan.pizza;
+
+
+/**
+ * Creates the New York Pizza.
+ * @author egan
+ *
+ */
+public class NewYorkPizza implements Pizza {
+
+	/* (non-Javadoc)
+	 * @see org.vegan.Pizza#pizzaName()
+	 */
+	@Override
+	public void pizzaName() {
+		System.out.println("New York Pizza");
+
+	}
+
+}
