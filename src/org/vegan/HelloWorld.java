4ceb66d2ca7a5913dd68b883f1cfeac63249ee5a
vintereg <vintereg@users.noreply.github.com>
2016-03-13 21:41:23 -0400

    Added Date Object

--- src/org/vegan/HelloWorld.java
+++ src/org/vegan/HelloWorld.java
@@ -3,6 +3,8 @@
  */
 package org.vegan;
 
+import java.util.Date;
+
 /**
  * @author egan
  * Class is designed to test github submission.
@@ -15,6 +17,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println("The date today is:  " + new Date());
 	}
 
 }
